b69349d0edf03abe9e157567918c8b713c1719b4
Alex <alernerm@gmail.com>
2019-04-11 13:46:25 -0400

    added shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 //import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [AppComponent], //CustomersComponent
-  imports: [BrowserModule, AppRoutingModule, CustomersModule],
+  imports: [BrowserModule, AppRoutingModule, CustomersModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
