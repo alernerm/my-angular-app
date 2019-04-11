62f50e6844a11477bc0aa52d590c37d9a07b542b
Alex <alernerm@gmail.com>
2019-04-11 10:46:51 -0400

    add files to git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CustomersModule } from './customers/customers.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+//import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent], //CustomersComponent
+  imports: [BrowserModule, AppRoutingModule, CustomersModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
